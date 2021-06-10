cf7111b662a3d63aa6a5f5a81e4f523fd66cee56
cosmicChimp <ian.t.marrs@gmail.com>
2021-06-10 02:03:03 -0600

    adjust profile pic to render to browser

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import PageHeader from '../PageHeader/PageHeader'
 import design from './About.module.css'
-import me from './../../img/Profile1-pic.jpg'
+// import me from './../../img/Profile1-pic.jpg'
 import {motion} from 'framer-motion'
 
 const About = () => {
@@ -17,7 +17,7 @@ const About = () => {
                         Although I love computers and the science behind them, I never found myself doing it professionally. My dad 
                         has been programming computers for 30 years and my brother is an IT Specialist and yet I never pictured myself being in the techincal industry... until now. 
                     </p>
-                    <p>
+                    <p className={design.AboutParagraph}>
                         A little over a year ago I found myself at a crossroads in my career and I had to choose whether to 
                         continue my career in a place where growth was all but exhausted or take a chance and go for something
                         that I enjoy and could potentially better family's lives. I decided the latter because although
@@ -25,7 +25,7 @@ const About = () => {
                         put my future in my hands and you can become more successful when you are free to make the decisions that are right for
                         you.
                     </p>
-                    <p>
+                    <p className={design.AboutParagraph}>
                         Stepping into the world of programming has bettered me as a person by making me look at all the of the
                         different ways things can be resolved as well as appreciate the finer things
                         in life we typically take for granted.
@@ -40,7 +40,7 @@ const About = () => {
                 </div>
                 </div>
                 <div className={design.Photo}>
-                    <motion.img className={design.Me} src={me} alt="myProfilePic"
+                    <motion.img className={design.Me} src={"https://dochub.com/ian-t-marrs/mqNjP3BVWoMYJEPV9yGzLk/profile1-pic-jpg?dt=jdo4Qw_RKy4tJ1GJmYMa"} alt="myProfilePic"
                         whileHover={{scale: 1.3, x: -30}}
                         initial={{opacity: 0, scale: 0, x: 100}}
                         animate={{opacity: 1, scale: 1, x: 0}}
